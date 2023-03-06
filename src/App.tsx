b33657529f21e806fecb5fc267dbc7f33fcc1e35
NuminousBlue <jdiehl2236@gmail.com>
2023-03-06 04:52:24 -0600

    title logo

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/Header";
+
+const App: React.FC<any> = () => {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Title.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { GiSkeletonKey } from "react-icons/gi";
+
+const CrossedKeys = () => {
+  return (
+    <div className="key-icon-container">
+      <GiSkeletonKey size="1rem" className="key-icon-left" />
+      <GiSkeletonKey size="1rem" className="key-icon-right" />
+    </div>
+  );
+};
+
+const Title = () => {
+  return (
+    <div className="title">
+      SKELETON
+      <div className="key-container">
+        <CrossedKeys />
+        <span className="key-title-text">KEY</span>
+        <CrossedKeys />
+      </div>
+    </div>
+  );
+};
+
+export default Title;
